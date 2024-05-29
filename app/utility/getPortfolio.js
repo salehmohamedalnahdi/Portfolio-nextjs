d704b0e8875aaa03f384c45972820e39e7de7227
salehmohamedalnahdi <smbt2011@gmail.com>
2024-05-29 19:10:57 +0300

    lajdkl

--- app/utility/getPortfolio.js
+++ app/utility/getPortfolio.js
@@ -1,21 +1,21 @@
 
 export default async function GetPortfolio (){
-        const response= await fetch('https://jsonplaceholder.typicode.com/users',{
+        const response= await fetch('https://personal-portfolio-backend-9t80.onrender.com/users',{
         next: {revalidate: 60}
     });
     const result= await response.json();
-    
+    const achievement= await result[0].achievements
     
     return(
         <div className="md:w-2/4">
-      <h4>Name: </h4>
-        <h4 >Age: </h4>
-        <h4 >Email: </h4>
+      <h4>Name: {result[0].name}</h4>
+        <h4 >Age: {result[0].age}</h4>
+        <h4 >Email: {result[0].email}</h4>
         <div>
           <h4 className="border-2 rounded-lg bg-red-200 ">Personal And Profiessinoal Achievements:</h4>
-          {result.map(i=>
-            <><li className="font-semibold ">title: {i.name}</li>
-            <li>Discription: {i.name}</li></>
+          {achievement.map(i=>
+            <><li className="font-semibold ">title: {i.title}</li>
+            <li>Discription: {i.content}</li></>
           )}
         </div>
     </div>
